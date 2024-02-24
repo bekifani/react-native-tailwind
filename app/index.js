6fab711e137f23903d5e51b9e333433705bdc65f
bekifani <bereketmesefen@gmail.com>
2024-02-24 14:19:05 +0300

    first commit

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+
+const Home = () => {
+    return (
+        <View>
+            <Text className="bg-gray-700 text-white text-2xl font-bold">HOme</Text>
+        </View>
+    )
+}
+export default Home
